5cdd2a1d53f64714aac67b62fb05bd8067293821
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-03-15 05:23:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,15 @@ function toggleLanguage(btn) {
     const isArabic = html.getAttribute('dir') === 'rtl';
 
     if (isArabic) {
-        // Switch to English
         html.setAttribute('dir', 'ltr');
         html.setAttribute('lang', 'en');
         btn.innerText = 'العربية';
     } else {
-        // Switch to Arabic
         html.setAttribute('dir', 'rtl');
         html.setAttribute('lang', 'ar');
         btn.innerText = 'English';
     }
 
-    // Update all text elements with data-en and data-ar attributes
     document.querySelectorAll('[data-en]').forEach(el => {
         el.innerText = isArabic ? el.getAttribute('data-en') : el.getAttribute('data-ar');
     });
